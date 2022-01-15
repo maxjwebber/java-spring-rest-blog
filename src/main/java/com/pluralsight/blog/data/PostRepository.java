d983654189d9f5438dad44e250e8054544f197b1
Max <max@maxwrit.es>
2022-01-15 17:25:22 -0500

    Added Author Relationships and Data

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -13,4 +13,5 @@ import org.springframework.data.rest.core.annotation.RestResource;
 public interface PostRepository extends JpaRepository<Post, Long> {
     @RestResource(rel="contains-title", path="containsTitle")
     public List<Post> findByTitleContaining(String title);
+    public List<Post> findByAuthor_Lastname(String lastname);
 }
